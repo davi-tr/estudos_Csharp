1190d5b8a78b8f9c86cf43d5ffb006364c6d8b03
Davi Trajano <davitrajano473@gmail.com>
2022-11-29 02:41:47 -0300

    Repositorio iniciado

---
+++ Models/Pessoa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudos.Models
+{
+    internal class Pessoa
+    {
+        public String Nome { get; set; }
+        public int Idade { get; set; }
+        
+        public void apresentar()
+        {
+            Console.WriteLine($"Olá, meu nome é {Nome}, e eu tenho {Idade}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using Estudos.Models;
+
+Pessoa p = new Pessoa();
+
+p.Nome = "Luis";
+p.Idade = 21;
+
+p.apresentar();
